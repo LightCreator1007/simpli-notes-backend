15438e9f2d0d16980b3f4d8fd613e08c4be6f6f4
Arunit <arunitchakraborty2006@gmail.com>
2025-08-19 00:14:17 +0530

    final

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://simpli-notes-2.vercel.app",
+    origin: process.env.CLIENT_URL || "https://simpli-notes-2.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
